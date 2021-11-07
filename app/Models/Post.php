a63aa98076c773b5d5028ce47b9b52b0cc2a49bf
amit34 <amit34@student.monash.edu>
2021-11-08 00:11:42 +1100

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
+use Illuminate\Support\Str;
+class Post extends Model
+{
+    use HasFactory;
+    public static function booted(){
+        static::creating(function (Post $post){
+          $post->uuid = Str::uuid();
+          if(!$post->slug){
+            $post->slug = $post->uuid;
+        }
+
+
+        });
+    }
+    protected $fillable = [
+    'uuid',
+    'title',
+    'body',
+    'slug',
+    'teaser',
+    'published'
+    ];
+}
